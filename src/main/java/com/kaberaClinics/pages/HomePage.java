081adb9fcf5b9aa70aaa78039903c3d183c4fdbf
AkashKabera <akash@kaberaglobal.com>
2020-10-05 18:55:37 +0530

    add scenario

--- src/main/java/com/kaberaClinics/pages/HomePage.java
+++ src/main/java/com/kaberaClinics/pages/HomePage.java
@@ -3,6 +3,7 @@ package com.kaberaClinics.pages;
 import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.WebElementFacade;
+import org.openqa.selenium.JavascriptExecutor;
 
 import java.util.concurrent.TimeUnit;
 
@@ -18,12 +19,62 @@ public class HomePage extends ExtendedPageObject {
     public void enterLocationAndSymptom(String symptom)throws InterruptedException{
         waitForPageLoaded();
         withTimeoutOf(15, TimeUnit.SECONDS).waitForPresenceOf(By.xpath("//input[@placeholder='I need treatment for ']"));
+        locationField.clear();
+        Thread.sleep(999);
         locationField.sendKeys("Mohali");
         waitForPageLoaded();
         needTreatmentField.sendKeys(symptom);
         waitForPageLoaded();
+        }
+    public void selectASymptomFromDropdown()throws InterruptedException{
+        waitForPageLoaded();
         withTimeoutOf(15, TimeUnit.SECONDS).waitForPresenceOf(By.xpath("//div/div[2]/div[1]/div/div[2]/ul/li[1]"));
         symptomsDropdown.click();
+    }
+
+    @FindBy(id = "firstmonth")
+    WebElementFacade oneMonthButton;
+    @FindBy(xpath = "//input[@placeholder='Enter 10 digit phone number']")
+    WebElementFacade phoneNumberField;
+    @FindBy(xpath = "(//input[@name='name'])[3]")
+    WebElementFacade patientNameField;
+    public void fillTheQuestionnaire(String buttonText, String buttonText1, String phoneNumber)throws InterruptedException{
+        withTimeoutOf(15, TimeUnit.SECONDS).waitForPresenceOf(By.xpath("//h5[@class='ques']"));
+        String xpathExpression = "//a[text()='"+buttonText+"']";
+        String xpathExpression1 = "(//button[text()='"+buttonText1+"'])[2]";
+        String name = "abc";
+        withTimeoutOf(15, TimeUnit.SECONDS).waitForPresenceOf(By.xpath(xpathExpression));
+        getDriver().findElement(By.xpath(xpathExpression)).click();
+        withTimeoutOf(15, TimeUnit.SECONDS).waitForPresenceOf(By.id("firstmonth"));
+        oneMonthButton.click();
+        withTimeoutOf(15, TimeUnit.SECONDS).waitForPresenceOf(By.xpath(xpathExpression1));
+        getDriver().findElement(By.xpath(xpathExpression1)).click();
+        waitForTextToAppear("Do You Have More Symptoms?");
+        withTimeoutOf(15, TimeUnit.SECONDS).waitForPresenceOf(By.xpath(xpathExpression));
+        getDriver().findElement(By.xpath(xpathExpression)).click();
+//        waitForTextToAppear("Type Your Symptoms and add it to your chart?.");
+        withTimeoutOf(15, TimeUnit.SECONDS).waitForPresenceOf(By.xpath(xpathExpression1));
+        getDriver().findElement(By.xpath(xpathExpression1)).click();
+        withTimeoutOf(15, TimeUnit.SECONDS).waitForPresenceOf(By.xpath(xpathExpression));
+        getDriver().findElement(By.xpath(xpathExpression)).click();
+        withTimeoutOf(15, TimeUnit.SECONDS).waitForPresenceOf(By.xpath("//input[@placeholder='Enter 10 digit phone number']"));
+        phoneNumberField.sendKeys(phoneNumber);
+        Thread.sleep(999);
+        getDriver().findElement(By.xpath("//a[text()='Next']")).click();
+        withTimeoutOf(15, TimeUnit.SECONDS).waitForPresenceOf(By.xpath("(//input[@name='name'])[3]"));
+        patientNameField.sendKeys(name);
+        Thread.sleep(999);
+        getDriver().findElement(By.xpath("//a[text()='Next']")).click();
+        Thread.sleep(4999);
+        getDriver().findElement(By.xpath("//a[text()='Ok']")).click();
+        }
+
+        public String getSuccessMessage()throws InterruptedException{
+        waitForPageLoaded();
+            JavascriptExecutor js=(JavascriptExecutor) getDriver();
+            String str=(String)js.executeScript("return document.getElementsByClassName('Toastify__toast-body')[0].innerText");
+            System.out.println("message is "+str);
+            return str;
         }
 
 }
--- src/test/java/com/fast/steps/serenity/EndUserSteps.java
+++ src/test/java/com/fast/steps/serenity/EndUserSteps.java
@@ -3,6 +3,9 @@ package com.fast.steps.serenity;
 import com.kaberaClinics.pages.*;
 import cucumber.api.DataTable;
 import net.thucydides.core.annotations.Step;
+import org.junit.Assert;
+
+import java.util.List;
 
 public class EndUserSteps {
 
@@ -23,14 +26,21 @@ public class EndUserSteps {
 
 	@Step
 	public void select_a_symptom_from_dropdown() throws Throwable{
+		homePage.selectASymptomFromDropdown();
 	}
 
 	@Step
 	public void fill_the_questionnaire_as_following(DataTable arg1) throws Throwable{
+		List<List<String>> data = arg1.raw();
+		String btn="Yes";
+		String btn1="Next";
+		String phone="2222222222";
+		homePage.fillTheQuestionnaire(btn,btn1,phone);
 	}
 
 	@Step
 	public void verify_that_success_message_displayed(String arg1) throws Throwable{
+		Assert.assertEquals("Thank you for your enquiry. Our professional will get in touch shortly.",homePage.getSuccessMessage());
 	}
 
 
